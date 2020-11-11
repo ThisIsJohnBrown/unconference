69e66b555978cbb272cdbe3f2b8b3658aeec78d8
John Brown <thisisjohnbrown@gmail.com>
2020-11-11 11:10:33 -0800

    sort sessions by time

--- src/components/Sessions.vue
+++ src/components/Sessions.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="container">
-    <div class="columns" v-for="(sessionChunk, i) in sessions" v-bind:key="i">
-      <div class="column" v-for="(session, j) in sessionChunk" v-bind:key="j">
-        <SessionInfoCard v-bind:session="session" />
+    <div
+      v-for="sessionTimePeriod in sessions.all"
+      v-bind:key="sessionTimePeriod.time"
+    >
+      <h3 class="is-size-3 m-4">{{ sessionTimePeriod.prettyTime }}</h3>
+      <div
+        class="columns"
+        v-for="(sessionChunk, i) in sessionTimePeriod.sessionGroup"
+        v-bind:key="i"
+      >
+        <div class="column" v-for="(session, j) in sessionChunk" v-bind:key="j">
+          <SessionInfoCard v-bind:session="session" />
+        </div>
       </div>
     </div>
   </div>
@@ -15,10 +25,41 @@ export default {
   name: "Sessions",
   computed: {
     sessions() {
-      if (this.$store.state.sessions) {
-        return chunk([...this.$store.state.sessions], 3);
-      }
-      return [[]];
+      const now = new Date();
+      const times = [
+        ...new Set(
+          this.$store.state.sessions.map(item => item.startTime.seconds)
+        )
+      ];
+      let sessionsByTime = times.map(time => {
+        return {
+          time,
+          sessions: []
+        };
+      });
+      this.$store.state.sessions.forEach(session => {
+        sessionsByTime[times.indexOf(session.startTime.seconds)].sessions.push(
+          session
+        );
+      });
+      const filteredSessionList = sessionsByTime.map(group => {
+        return {
+          time: group.time,
+          //eslint-disable-next-line
+          prettyTime: (new Date(group.time * 1000)).toTimeString().split(' ')[0],
+          sessionGroup: chunk(group.sessions, 3)
+        };
+      });
+      console.log(filteredSessionList);
+      return {
+        all: filteredSessionList,
+        expired: filteredSessionList.filter(
+          session => now > new Date(session.time * 1000)
+        ),
+        upcoming: filteredSessionList.filter(
+          session => now <= new Date(session.time * 1000)
+        )
+      };
     }
   },
   components: {
