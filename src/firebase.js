967c7a6e213d76fb0ec5b83acfd6b1d402eb8779
John Brown <thisisjohnbrown@gmail.com>
2020-11-18 20:40:38 -0800

    update process envs

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,17 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/analytics";
-import * as config from "../firebase.config";
 
-const fb = firebase.initializeApp(config);
+const fb = firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.VUE_APP_FIRESTORE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIRESTORE_DATABASEURL,
+  projectId: process.env.VUE_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIRESTORE_APP_ID,
+  measurementId: process.env.VUE_APP_FIRESTORE_MEASUREMENT_ID
+});
 fb.analytics();
 
 const db = fb.firestore();
