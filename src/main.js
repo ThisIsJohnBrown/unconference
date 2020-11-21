691b9403835f30e6e431fdf42e566e464b75436b
John Brown <thisisjohnbrown@gmail.com>
2020-11-21 13:49:05 -0800

    update vuetify to use scss variables

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import vuetify from "@/plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import firebase from "firebase/app";
 import AsyncComputed from "vue-async-computed";
+import vuetify from "./plugins/vuetify";
 
 Vue.use(AsyncComputed);
 
